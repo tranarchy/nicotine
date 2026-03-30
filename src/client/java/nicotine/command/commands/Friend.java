2b19a196368cbef1dad2e0b4824db19f82de546c
tranarchy <150830424+tranarchy@users.noreply.github.com>
2026-03-30 22:57:57 +0200

    friend command use mc services

--- src/client/java/nicotine/command/commands/Friend.java
+++ src/client/java/nicotine/command/commands/Friend.java
@@ -1,13 +1,14 @@
 package nicotine.command.commands;
 
-import net.minecraft.client.player.AbstractClientPlayer;
-import net.minecraft.client.player.RemotePlayer;
+import net.minecraft.server.players.NameAndId;
 import net.minecraft.world.entity.Entity;
 import nicotine.command.Command;
 import nicotine.events.ClientLevelTickEvent;
 import nicotine.util.EventBus;
 import nicotine.util.Message;
 import nicotine.util.Settings;
+
+import java.util.Optional;
 
 import static nicotine.util.Common.friendList;
 import static nicotine.util.Common.mc;
@@ -26,30 +27,22 @@ public class Friend extends Command {
         }
 
         if (splitCommand[1].equals("add") || splitCommand[1].equals("remove")) {
-            AbstractClientPlayer friend = null;
+            Optional<NameAndId> nameAndIdOptional = mc.services().nameToIdCache().get(splitCommand[2]);
 
-            for (AbstractClientPlayer player : mc.level.players()) {
-                if (!(player instanceof RemotePlayer))
-                    continue;
-
-                if (player.getName().getString().equalsIgnoreCase(splitCommand[2])) {
-                    friend = player;
-                    break;
-                }
-            }
-
-            if (friend == null) {
-                Message.sendWarning("Player is not online!");
+            if (!nameAndIdOptional.isPresent()) {
+                Message.sendWarning("Player doesn't exist");
                 return;
             }
 
+            NameAndId friend = nameAndIdOptional.get();
+
             if (splitCommand[1].equals("add")) {
-                friendList.add(friend.getUUID());
-                Message.sendInfo(String.format("Added %s to friend list!", friend.getName().getString()));
+                friendList.add(friend.id());
+                Message.sendInfo(String.format("Added %s to friend list!", friend.name()));
             } else {
-                if (friendList.contains(friend.getUUID())) {
-                    friendList.remove(friend.getUUID());
-                    Message.sendWarning(String.format("Removed %s from friend list!", friend.getName().getString()));
+                if (friendList.contains(friend.id())) {
+                    friendList.remove(friend.id());
+                    Message.sendWarning(String.format("Removed %s from friend list!", friend.name()));
                 } else {
                     Message.sendWarning("Player is not in friend list!");
                     return;
